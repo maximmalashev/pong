e103f481cf03787d1e0899db1f2cbb903a38b0a4
unknown <maxim.malashev@gmail.com>
2020-07-25 14:35:48 +0200

    added view matrix

--- src/rendering/window.cpp
+++ src/rendering/window.cpp
@@ -11,8 +11,8 @@ Window::Window(int width, int height, std::string title)
 		std::exit(-1);
 	}
 
-	glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
 	glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
+	glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
 	glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
 	glfwWindowHint(GLFW_OPENGL_FORWARD_COMPAT, GL_TRUE);
 
